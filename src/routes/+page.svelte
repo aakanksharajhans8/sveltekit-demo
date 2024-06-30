16b6a2d6e1c18d447be9cc61e90a7992b7545eee
Aakanksha Rajhans <aakanksharajhansl@gmail.com>
2024-07-01 00:52:56 +0530

    added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="mt-5 mb-5 hover:underline font-bold hover:text-red-500">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
